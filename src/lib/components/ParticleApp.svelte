e638e24693c3dbcd974c2821d0fd6e01c34a85a1
Maarten <maartenzam@gmail.com>
2024-12-13 12:01:40 +0100

    Basic treemap

--- src/lib/components/ParticleApp.svelte
+++ src/lib/components/ParticleApp.svelte
@@ -7,6 +7,7 @@
 	import { Stats } from 'pixi-stats';
 	import Intro from './vis/Intro.svelte';
 	import BarChart from './vis/BarChart.svelte';
+	import TreeMap from './vis/TreeMap.svelte';
 	import AllIndicatorMap from './vis/AllIndicatorMap.svelte';
 	import AllIndicatorMatrix from './vis/AllIndicatorMatrix.svelte';
 	import SVGContainer from './VisContainer.svelte';
@@ -28,7 +29,8 @@
 
 	let modes = [
 		{ vis: Intro, sceneCount: 1 },
-		{ vis: BarChart, sceneCount: 3},
+		{ vis: BarChart, sceneCount: 3 },
+		{ vis: TreeMap, sceneCount: 1 },
 		{ vis: AllIndicatorMap, sceneCount: 2 },
 		{ vis: AllIndicatorMatrix, sceneCount: 2 },
 		{ vis: AllIndicatorMap, sceneCount: 1 },
--- src/lib/components/vis/TreeMap.svelte
+++ src/lib/components/vis/TreeMap.svelte
@@ -1,30 +1,51 @@
 <script>
+    import { countryRegions as countryRegions } from '$lib/data/country-regions.js';
 	import { nodeState, labelState } from '$lib/state/nodeState.svelte.js';
 	import VisContainer from '../VisContainer.svelte';
 	import { goHome, resetColor } from '$lib/utils/particleUtils.js';
     import ISOCodeLabels from '$lib/components/vis/ISOCodeLabels.svelte';
+    import { hierarchy, treemap } from 'd3'
 
     let { inView = false, activeScene = 0,w, h, frame } = $props();
 
 	let particles = nodeState.nodes;
     let indicatorParticles = particles.filter(d => d.indicatorName == "GDP per capita, PPP (constant 2021 international $)")
+        /*.map(d => ({...d,
+            region: countryRegions.find(c => c.iso3c == d.country).region_iso3c
+        }))*/
     let otherParticles = particles.filter(d => d.indicatorName != "GDP per capita, PPP (constant 2021 international $)")
 
+    const regions = ["EAS", "ECS", "LCN", "MEA", "SAS", "SSF"].map(r => ({
+        "name": r,
+        "children": indicatorParticles.filter(d => countryRegions.find(c => c.iso3c == d.country).region_iso3c == r)
+    }))
+    let world = {
+        "name": "world",
+        children: regions
+    }
+
     const margins = {
-		top: 160,
+		top: 20,
 		right: 20,
-		bottom: 120,
+		bottom: 20,
 		left: 120
 	};
 
-    const sceneConfig = {
+    let root = hierarchy(world)
+    root.sum((d) => d.valueAbs);
+    root.sort((a, b) => b.height - a.height || b.valueAbs - a.valueAbs);
+    
+    treemap().size([w - margins.right - margins.left,h - margins.top - margins.bottom]).padding(2)(root)
+    console.log(root.leaves())
+
+    /*const sceneConfig = {
 		0: { sortmode: 'none', scaling: 'none' },
 		1: { sortmode: 'levels', scaling: 'none' },
         2: { sortmode: 'levels', scaling: 'value' },
 
 	};
     let sortmode = $derived(sceneConfig[activeScene].sortmode)
-    let scalemode = $derived(sceneConfig[activeScene].scaling)
+    let scalemode = $derived(sceneConfig[activeScene].scaling)*/
 
 	$effect(() => {
 		if (inView === true && frame) {
@@ -40,17 +61,23 @@
 			node.scaleY = 10;
 			resetColor(node);
         })
-        indicatorParticles.sort((a,b) => 
-            sortmode == "none"
-                ? 0
-                : b.level - a.level
-        ).forEach((d, i) => {
-            d.x = margins.left + (i) * RADIUS + i*2;
-			d.y = scalemode == 'value' ? h * 0.4 - d.valuePP/100 : h * 0.4;
-            d.scaleX = RADIUS;
-            d.scaleY = scalemode == 'value' ? d.valuePP/100 : RADIUS;
+        indicatorParticles.forEach((d) => {
+            let countryNode = root.leaves().find(c => c.data.country == d.country)
+            if(countryNode){
+                console.log(countryNode.x0)
+                d.x = margins.left + countryNode.x0;
+			    d.y = h + margins.bottom - countryNode.y0;
+                d.scaleX = countryNode.x1 - countryNode.x0;
+                d.scaleY = countryNode.y0 - countryNode.y1;
+            }
+            else{
+                goHome(d);
+			    d.scaleX = 10;
+			    d.scaleY = 10;
+			    resetColor(d);
+            }
 
-            const countryLabel = labelState.labels[d.country]
+            /*const countryLabel = labelState.labels[d.country]
             if(scalemode == 'value' && (d.valuePP > 5000 || d.valuePP < 0)){
                 countryLabel.x = margins.left + (i) * RADIUS + i*2;
                 countryLabel.y = h * 0.4 - d.valuePP/100
@@ -58,7 +85,7 @@
             else {
                 countryLabel.x = countryLabel.homepoint.x;
 				countryLabel.y = countryLabel.homepoint.y;
-            }
+            }*/
         })
 	}
 </script>
