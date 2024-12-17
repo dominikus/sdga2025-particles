bee1da86967498c42faa3f705e1a4e2d70c3888c
Alice <alice.thudt@gmail.com>
2024-12-17 13:58:13 +0100

    legends in new visualizations and labels without data placed correctly

--- src/lib/components/vis/BarChart.svelte
+++ src/lib/components/vis/BarChart.svelte
@@ -2,29 +2,34 @@
 	import { nodeState, labelState } from '$lib/state/nodeState.svelte.js';
 	import VisContainer from '../VisContainer.svelte';
 	import { goHome, resetColor } from '$lib/utils/particleUtils.js';
-    import ISOCodeLabels from '$lib/components/vis/ISOCodeLabels.svelte';
+	import ISOCodeLabels from '$lib/components/vis/ISOCodeLabels.svelte';
+	import { grid as countries } from '$lib/data/worldtilegrid.js';
+	import LevelLegend from '../general/LevelLegend.svelte';
 
-    let { inView = false, activeScene = 0,w, h, frame } = $props();
+	let { inView = false, activeScene = 0, w, h, frame } = $props();
 
 	let particles = nodeState.nodes;
-    let indicatorParticles = particles.filter(d => d.indicatorName == "GDP per capita, PPP (constant 2021 international $)")
-    let otherParticles = particles.filter(d => d.indicatorName != "GDP per capita, PPP (constant 2021 international $)")
+	let indicatorParticles = particles.filter(
+		(d) => d.indicatorName == 'GDP per capita, PPP (constant 2021 international $)'
+	);
+	let otherParticles = particles.filter(
+		(d) => d.indicatorName != 'GDP per capita, PPP (constant 2021 international $)'
+	);
 
-    const margins = {
-		top: 160,
+	const margins = {
+		top: 180,
 		right: 20,
 		bottom: 120,
 		left: 120
 	};
 
-    const sceneConfig = {
+	const sceneConfig = {
 		0: { sortmode: 'none', scaling: 'none' },
 		1: { sortmode: 'levels', scaling: 'none' },
-        2: { sortmode: 'levels', scaling: 'value' },
-
+		2: { sortmode: 'levels', scaling: 'value' }
 	};
-    let sortmode = $derived(sceneConfig[activeScene].sortmode)
-    let scalemode = $derived(sceneConfig[activeScene].scaling)
+	let sortmode = $derived(sceneConfig[activeScene].sortmode);
+	let scalemode = $derived(sceneConfig[activeScene].scaling);
 
 	$effect(() => {
 		if (inView === true && frame) {
@@ -32,41 +37,46 @@
 		}
 	});
 
-    const RADIUS = (w - margins.left - margins.right - indicatorParticles.length*2)/indicatorParticles.length
+	const RADIUS =
+		(w - margins.left - margins.right - indicatorParticles.length * 2) / indicatorParticles.length;
 	function layout() {
-        otherParticles.forEach(node => {
-            goHome(node);
+		countries.forEach((country) => {
+			const countryLabel = labelState.labels[country.iso3c];
+			countryLabel.x = countryLabel.homepoint.x;
+			countryLabel.y = countryLabel.homepoint.y;
+		});
+
+		otherParticles.forEach((node) => {
+			goHome(node);
 			node.scaleX = 10;
 			node.scaleY = 10;
 			resetColor(node);
-        })
-        indicatorParticles.sort((a,b) => 
-            sortmode == "none"
-                ? 0
-                : b.level - a.level
-        ).forEach((d, i) => {
-            d.x = margins.left + (i) * RADIUS + i*2;
-			d.y = scalemode == 'value' ? h * 0.4 - d.valuePP/100 : h * 0.4;
-            d.scaleX = RADIUS;
-            d.scaleY = scalemode == 'value' ? d.valuePP/100 : RADIUS*4;
+		});
 
-            const countryLabel = labelState.labels[d.country]
-            if(scalemode == 'value' && (d.valuePP > 12000 || d.valuePP < -4000)){
-                countryLabel.x = margins.left + (i) * RADIUS + i*2 + 4;
-                countryLabel.y = d.valuePP < 0 ? h * 0.4 - d.valuePP/100 + 12 : h * 0.4 - d.valuePP/100 - 4
-            }
-            else {
-                countryLabel.x = countryLabel.homepoint.x;
-				countryLabel.y = countryLabel.homepoint.y;
-            }
-        })
+		indicatorParticles
+			.sort((a, b) => (sortmode == 'none' ? 0 : b.level - a.level))
+			.forEach((d, i) => {
+				d.x = margins.left + i * RADIUS + i * 2;
+				d.y = scalemode == 'value' ? h * 0.4 - d.valuePP / 100 : h * 0.4;
+				d.scaleX = RADIUS;
+				d.scaleY = scalemode == 'value' ? d.valuePP / 100 : RADIUS * 4;
+
+				const countryLabel = labelState.labels[d.country];
+				if (scalemode == 'value' && (d.valuePP > 12000 || d.valuePP < -4000)) {
+					countryLabel.x = margins.left + i * RADIUS + i * 2 + 4;
+					countryLabel.y =
+						d.valuePP < 0 ? h * 0.4 - d.valuePP / 100 + 12 : h * 0.4 - d.valuePP / 100 - 4;
+				}
+			});
 	}
 </script>
 
 <VisContainer {w} {h}>
-    <ISOCodeLabels {w} h={2500} slot="iso-code-labels"></ISOCodeLabels>
-	<g slot="svg">
-	</g>
+	<ISOCodeLabels {w} h={2500} slot="iso-code-labels"></ISOCodeLabels>
+	<g slot="svg"> </g>
+	<div slot="top">
+		<LevelLegend indicator=" in GDP per capita, PPP (constant 2021 international $)"></LevelLegend>
+	</div>
 </VisContainer>
 
 <style>
--- src/lib/components/vis/TreeMap.svelte
+++ src/lib/components/vis/TreeMap.svelte
@@ -1,42 +1,49 @@
 <script>
-    import { countryRegions as countryRegions } from '$lib/data/country-regions.js';
+	import { countryRegions as countryRegions } from '$lib/data/country-regions.js';
 	import { nodeState, labelState } from '$lib/state/nodeState.svelte.js';
 	import VisContainer from '../VisContainer.svelte';
 	import { goHome, resetColor } from '$lib/utils/particleUtils.js';
-    import ISOCodeLabels from '$lib/components/vis/ISOCodeLabels.svelte';
-    import { hierarchy, treemap, treemapBinary } from 'd3'
+	import { grid as countries } from '$lib/data/worldtilegrid.js';
+	import ISOCodeLabels from '$lib/components/vis/ISOCodeLabels.svelte';
+	import { hierarchy, treemap, treemapBinary } from 'd3';
+	import LevelLegend from '../general/LevelLegend.svelte';
 
-    let { inView = false, w, h, frame } = $props();
+	let { inView = false, w, h, frame } = $props();
 
 	let particles = nodeState.nodes;
-    let indicatorParticles = particles.filter(d => d.indicatorName == "GDP per capita, PPP (constant 2021 international $)")
-    let otherParticles = particles.filter(d => d.indicatorName != "GDP per capita, PPP (constant 2021 international $)")
+	let indicatorParticles = particles.filter(
+		(d) => d.indicatorName == 'GDP per capita, PPP (constant 2021 international $)'
+	);
+	let otherParticles = particles.filter(
+		(d) => d.indicatorName != 'GDP per capita, PPP (constant 2021 international $)'
+	);
 
-    const margins = {
-		top: 20,
+	const margins = {
+		top: 160,
 		right: 20,
 		bottom: 20,
 		left: 120
 	};
 
-    const regions = ["EAS", "ECS", "LCN", "MEA", "SAS", "SSF"].map(r => ({
-        "name": r,
-        "children": indicatorParticles.filter(d => countryRegions.find(c => c.iso3c == d.country).region_iso3c == r)
-    }))
-    let world = {
-        "name": "world",
-        children: regions
-    }
+	const regions = ['EAS', 'ECS', 'LCN', 'MEA', 'SAS', 'SSF'].map((r) => ({
+		name: r,
+		children: indicatorParticles.filter(
+			(d) => countryRegions.find((c) => c.iso3c == d.country).region_iso3c == r
+		)
+	}));
+	let world = {
+		name: 'world',
+		children: regions
+	};
 
-    let root = hierarchy(world)
-    root.sum((d) => d.valueAbs);
-    root.sort((a, b) => a.value - b.value);
-    
-    treemap()
-        .size([w - margins.right - margins.left,h - margins.top - margins.bottom])
-        .padding(2)
-        .tile(treemapBinary)
-        (root)
+	let root = hierarchy(world);
+	root.sum((d) => d.valueAbs);
+	root.sort((a, b) => a.value - b.value);
+
+	treemap()
+		.size([w - margins.right - margins.left, h - margins.top - margins.bottom])
+		.padding(2)
+		.tile(treemapBinary)(root);
 
 	$effect(() => {
 		if (inView === true && frame) {
@@ -44,48 +51,53 @@
 		}
 	});
 
-    const RADIUS = 10
+	const RADIUS = 10;
 	function layout() {
-        otherParticles.forEach(node => {
-            goHome(node);
+		countries.forEach((country) => {
+			const countryLabel = labelState.labels[country.iso3c];
+			countryLabel.x = countryLabel.homepoint.x;
+			countryLabel.y = countryLabel.homepoint.y;
+		});
+
+		otherParticles.forEach((node) => {
+			goHome(node);
 			node.scaleX = RADIUS;
 			node.scaleY = RADIUS;
 			resetColor(node);
-        })
-        indicatorParticles.forEach((d) => {
-            let countryNode = root.leaves().find(c => c.data.country == d.country)
-            const countryLabel = labelState.labels[d.country]
-            if(countryNode){
-                d.x = margins.left + countryNode.x0;
-			    d.y = h + margins.bottom - countryNode.y0;
-                d.scaleX = countryNode.x1 - countryNode.x0;
-                d.scaleY = countryNode.y0 - countryNode.y1;
-                if(d.valueAbs > 10000){
-                    countryLabel.x = margins.left + countryNode.x0 + 16;
-                    countryLabel.y = h + margins.bottom - countryNode.y1 + 16;
-                }
-                else {
-                    countryLabel.x = countryLabel.homepoint.x;
-				    countryLabel.y = countryLabel.homepoint.y;
-                }
-            }
-            else {
-                goHome(d);
-			    d.scaleX = 10;
-			    d.scaleY = 10;
-			    resetColor(d);
+		});
+		indicatorParticles.forEach((d) => {
+			let countryNode = root.leaves().find((c) => c.data.country == d.country);
+			const countryLabel = labelState.labels[d.country];
+			if (countryNode) {
+				d.x = margins.left + countryNode.x0;
+				d.y = h + margins.bottom - countryNode.y0;
+				d.scaleX = countryNode.x1 - countryNode.x0;
+				d.scaleY = countryNode.y0 - countryNode.y1;
+				if (d.valueAbs > 10000) {
+					countryLabel.x = margins.left + countryNode.x0 + 16;
+					countryLabel.y = h + margins.bottom - countryNode.y1 + 16;
+				} else {
+					countryLabel.x = countryLabel.homepoint.x;
+					countryLabel.y = countryLabel.homepoint.y;
+				}
+			} else {
+				goHome(d);
+				d.scaleX = 10;
+				d.scaleY = 10;
+				resetColor(d);
 
-                countryLabel.x = countryLabel.homepoint.x;
+				countryLabel.x = countryLabel.homepoint.x;
 				countryLabel.y = countryLabel.homepoint.y;
-            }
-        })
+			}
+		});
 	}
 </script>
 
 <VisContainer {w} {h}>
-    <ISOCodeLabels {w} h={2500} slot="iso-code-labels"></ISOCodeLabels>
-	<g slot="svg">
-	</g>
+	<ISOCodeLabels {w} h={2500} slot="iso-code-labels"></ISOCodeLabels>
+	<div slot="top">
+		<LevelLegend indicator=" in GDP per capita, PPP (constant 2021 international $)"></LevelLegend>
+	</div>
 </VisContainer>
 
 <style>
